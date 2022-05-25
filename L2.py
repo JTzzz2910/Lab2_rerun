0b8549d3e8a51042963254b63ea2dd803141329c
JTzzz2910 <JTZC29.21@ichat.sp.edu.sg>
2022-05-26 00:57:33 +0800

    Yes

--- L2.py
+++ L2.py
@@ -1,1 +1,32 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5,67, 32)")
+
+
+def get_user_input():
+    lists = []
+    nums = input()
+    lists = nums.split(",")
+    for i in lists:
+        lists.append(float(i))
+    return lists
+
+
+def calc_average(lists):
+    total = 0
+    for i in lists:
+        total += i
+
+    return float(total / len(lists))
+
+
+def calc_min_max_temp(lists):
+    return [int(min(lists)), int(max(lists))]
+
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+
+
+if __name__ == "__main__":
+    main()
